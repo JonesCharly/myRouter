cc010010fb30dac0baa05a1f2599a47573ff7798
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-08-14 14:27:58 +0200

    initial quetes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './Home';
+import History from './History'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>coucou</p>
+        <BrowserRouter>
+          <NavLink exact to="/" > Acceuil </NavLink>
+          <NavLink exact to="/notre-histoire" activeClassName="selected"> About us </NavLink>
+
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/notre-histoire" component={History}/>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/History.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class History extends Component {
+
+    render(){
+
+        return(
+            <div>
+                <h1>Notre histoire</h1>
+            </div>
+        )
+    }
+}
+
+export default History;
